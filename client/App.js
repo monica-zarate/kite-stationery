00346818f29c8d49eb599abd3a758b00e3de31db
Karina Song <ksysong@gmail.com>
2023-03-25 16:47:27 -0700

    setup skeleton and bottom nav

--- client/App.js
+++ client/App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Import the SafeAreaProvider
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+// Import Icons
+import Icon from 'react-native-vector-icons/Ionicons';
+
+// React native navigation components 
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+// Instantiate the Bottom Tab component 
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+        <NavigationContainer>
+            <Tab.Navigator
+                  initialRouteName='Home'
+                  screenOptions={{
+                      tabBarActiveTintColor: '#222222',
+                      tabBarInactiveTintColor: '#939393',
+                      headerShown: false
+                  }}
+            >
+                <Tab.Screen 
+                    name="Home"
+                    component={HomeScreen}
+                    options={{
+                        tabBarLabel: 'Home',
+                        tabBarIcon: ({ color, size }) => (
+                            <Icon
+                                name="home" 
+                                color={color} 
+                                size={size} 
+                            />
+                        ),
+                    }}
+                />
+                 <Tab.Screen 
+                    name="Search"
+                    component={SearchNav}
+                    options={{
+                        tabBarLabel: 'Search',
+                        tabBarIcon: ({ color, size }) => (
+                            <Icon
+                                name="search" 
+                                color={color} 
+                                size={size} 
+                            />
+                        ),
+                    }}
+                />
+                 <Tab.Screen 
+                    name="Account"
+                    component={AccountScreen}
+                    options={{
+                        tabBarLabel: 'Account',
+                        tabBarIcon: ({ color, size }) => (
+                            <Icon
+                                name="person" 
+                                color={color} 
+                                size={size} 
+                            />
+                        ),
+                    }}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    </SafeAreaProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
