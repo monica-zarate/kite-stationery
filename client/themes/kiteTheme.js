70e2298805ad846d6ef0394072a292d248b4ba93
Karina Song <ksysong@gmail.com>
2023-03-29 22:45:09 -0700

    complete home screen

--- client/themes/kiteTheme.js
+++ client/themes/kiteTheme.js
@@ -1,9 +1,10 @@
 import { createTheme } from '@rneui/themed';
 
-const themePalette = {
-    primary: '#FF9F00',
+export const themePalette = {
+    primary: '#FF5A00',
     secondary: '#0B70FE',
-    tertiary: '#FF5A00',
+    tertiary: '#FF9F00',
+    accent: '#43CCFF',
     white: '#fff',
     grey: '#939393',
 };
@@ -36,7 +37,8 @@ export const kiteTheme = createTheme({
                 fontWeight: 'normal',
                 fontFamily: 'Inter_600SemiBold',
                 fontSize: 26,
-                lineHeight: 29
+                lineHeight: 29,
+                // marginBottom: 24
             },
             h3Style: {
                 fontWeight: 'normal',
