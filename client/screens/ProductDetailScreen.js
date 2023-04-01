83a44984b0189c8a1d4c6a6d468271d3521319a2
Karina Song <ksysong@gmail.com>
2023-04-01 02:23:19 -0700

    create back button

--- client/screens/ProductDetailScreen.js
+++ client/screens/ProductDetailScreen.js
@@ -1,25 +1,34 @@
-import { View, StyleSheet, Pressable } from 'react-native';
+import { View, StyleSheet, Pressable, ImageBackground } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { Text,Image, Button } from '@rneui/themed';
+import { fonts } from '@rneui/base';
 
 export default function ProductDetailScreen({ route, navigation }) {
 
     const {product_id} = route.params;
     return (
         <View style={styles.container}>
-            <Image 
+            <ImageBackground 
                 source={{uri: "https://i.imgur.com/agofhX4.jpg"}}
                 style={styles.product_img}
-            />
+            >
+                <Pressable
+                    title={'Go Back'}
+                    onPress={() => navigation.goBack()}
+                    style={styles.backContainer}
+                >
+                    <Icon name="arrow-back-outline" style={styles.backIcon}  />
+                </Pressable>
+            </ImageBackground>
             <View style={styles.product_container}>
                 <View style={styles.product_title}>
                     <Text h2 style={{marginBottom: 12}}>Moon & Smith Floral Notebook</Text>
-                    <View style={styles.heartContainer}>
+                    <Pressable style={styles.heartContainer}>
                         <Icon 
                             name="heart-outline"
                             style={styles.heart}
                         />
-                    </View>
+                    </Pressable>
                 </View>
                 <Text h3 style={{marginBottom: 30}}>$24.99</Text>
                 <Text h4 style={{marginBottom: 8}}>Description</Text>
@@ -47,6 +56,25 @@ const styles = StyleSheet.create({
     container: {
         flex: 1, 
         backgroundColor: '#fff',
+    },
+    backContainer: {
+        // borderRadius: 50
+        width: 50,
+        height: 50,
+        borderRadius: 50,
+        textAlign: "center",
+        // backgroundColor: "red",
+        marginTop: 30,
+        marginLeft: 10,
+        alignItems: "center"
+    },
+    backIcon: {
+        backgroundColor: "#fff",
+        // alignSelf: "center",
+        padding: 10,
+        borderRadius: 50,
+        fontSize: 32,
+        // textAlignVertical: "center"
     },
     product_img: {
         width: "100%",
