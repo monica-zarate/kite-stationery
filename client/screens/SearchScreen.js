4e5235de28d368f68ac0a75ecca9b59614084b5b
Karina Song <ksysong@gmail.com>
2023-04-02 16:10:48 -0700

    setup brand fetch with live api

--- client/screens/SearchScreen.js
+++ client/screens/SearchScreen.js
@@ -1,4 +1,5 @@
-import { View, StyleSheet, FlatList, Pressable } from 'react-native';
+import { useState, useEffect } from 'react';
+import { View, StyleSheet, FlatList, Pressable, ActivityIndicator } from 'react-native';
 import { Text, SearchBar, Image } from '@rneui/themed';
 
 import Header from '../components/Header';
@@ -7,35 +8,121 @@ import ProductCategoriesFetch from '../components/ProductCategoriesFetch';
 
 export default function SearchScreen({ navigation }) {
 
+    // This fetch will be getting the brands 
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [dataResult, setDataResult] = useState([]);
+
+    useEffect(() => {
+        const uri = 'http://kite-stationery.monicasites.com/api/v1/brands/read.php'; 
+
+        fetch(uri)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setDataResult(result);
+                    setIsLoaded(true);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, []);
+
     return (
         <View style={styles.container}>
-            {/* <FlatList /> */}
             <Header />    
-            <Text h1 style={{marginVertical: 30}}>What item are you looking for?</Text>
-            <SearchBar 
-                lightTheme={true}
-                containerStyle={{
-                    backgroundColor: '#fff',
-                    borderBottomColor: 'transparent',
-                    borderTopColor: 'transparent',
-                    marginBottom: 30
-                }}
-                inputContainerStyle={{
-                    backgroundColor: '#fff',
-                    color: 'salmon'
-                }}
-                placeholder={"Search by Product Name"}
-            />
-            <Text h2 style={{marginBottom: 24}}>Product Categories</Text>
-            <ProductCategoriesFetch 
-                navigation={navigation}
-            />
-            <Text h2 style={{marginBottom: 24}}>Brands</Text>
-            <BrandFetch 
-                navigation={navigation}
-            />
+            
+            {displayDataContainer(error, isLoaded, dataResult)}
+            
         </View>
     )
+};
+
+function displayDataContainer (error, isLoaded, dataResult, navigation) {
+    
+    const renderItem = ({ item }) => (
+        <Pressable 
+            style={styles.brand}
+            onPress={() => navigation.navigate('Results', {
+                brand_id: item.brand_id,
+            })}
+        >
+            <Image 
+                source={{uri: item.brand_icon}}
+                containerStyle={styles.brand_icon}
+                resizeMode="contain"
+            />
+            <Text style={styles.brand_name}>{item.brand_name}</Text>
+        </Pressable>
+    );
+
+    if (error) {
+        // Show error message 
+        return (
+            <View>
+                <Text>Error: {error.message}</Text>
+            </View>
+        );
+    };
+
+    if (!isLoaded) {
+        // Show activity indicator while the request is loading
+        return (
+            <View>
+                <Text>Loading...</Text>
+                <ActivityIndicator size="large" color="#d55140"/>
+            </View>
+        );
+    };
+
+    if (dataResult.count === 0) {
+        // If there are no records, then this screen will show
+        return (
+            <View>
+                <Text>No records found for search </Text>
+            </View>
+        );
+    };
+
+    // Returned flatlist will display the retrieved data
+    return (
+        <FlatList 
+            ListHeaderComponent={
+                <>
+                    <Text h1 style={{marginVertical: 30}}>What item are you looking for?</Text>
+                    <SearchBar 
+                        lightTheme={true}
+                        containerStyle={{
+                            backgroundColor: '#fff',
+                            borderBottomColor: 'transparent',
+                            borderTopColor: 'transparent',
+                            marginBottom: 30
+                        }}
+                        inputContainerStyle={{
+                            backgroundColor: '#fff',
+                            color: 'salmon'
+                        }}
+                        placeholder={"Search by Product Name"}
+                    />
+                    <Text h2 style={{marginBottom: 24}}>Product Categories</Text>
+                    <ProductCategoriesFetch 
+                        navigation={navigation}
+                    />
+                    <Text h2 style={{marginBottom: 24}}>Brands</Text>
+                </>
+            }
+            data={dataResult.brands}
+            renderItem={renderItem}
+            keyExtractor={item => item.brand_id}
+            numColumns={3}
+            style={{ width: '100%', marginBottom: 24}}
+            columnWrapperStyle={{ justifyContent: 'space-between', gap: 7 }}
+            navigation={navigation}
+        />
+    );  
+    
 };
 
 const styles = StyleSheet.create({
@@ -44,4 +131,27 @@ const styles = StyleSheet.create({
         marginHorizontal: 24,
         marginTop: 15
     },
+    brand: {
+        backgroundColor: "#fff",
+        padding: 18,
+        borderRadius: 4,
+        alignItems: "center",
+        width: "32%",
+        marginBottom: 7,
+    },
+    brand_icon: {
+        height: 40,
+        width: 40,
+        marginBottom: 10,
+        // resizeMode: "cover"
+        // width: '100%',
+        // flex: 1,
+    },
+    brand_name: {
+        textAlignVertical: "center",
+        textAlign: "center",
+        textTransform: "uppercase",
+        fontFamily: 'Inter_600SemiBold',
+        fontSize: 11
+    }
 });
