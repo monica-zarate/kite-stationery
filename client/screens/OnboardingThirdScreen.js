451f4fe375a46f67bc56a83bcb35d47f56badf32
Karina Song <ksysong@gmail.com>
2023-04-06 23:52:17 -0700

    setup 3rd onboarding screen

--- client/screens/OnboardingThirdScreen.js
+++ client/screens/OnboardingThirdScreen.js
@@ -1,15 +1,68 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View} from 'react-native';
+import { Text, Button, Image } from '@rneui/themed';
 
-export default function OnboardingThirdScreen() {
+import { themePalette } from "../themes/kiteTheme";
+
+export default function OnboardingThirdScreen({setShowOnboarding}) {
 
     return (
-        <View>
-            <Text>This is the third screen</Text>
+        <View style={styles.container}>
+            <Image 
+                source={require("../assets/onboard-updates.png")}
+                style={styles.onboardImg}
+            />
+            <Text h1>Get Updates!</Text>
+            <Text style={{fontSize: 21, color: "#757575", textAlign: "center", marginBottom: 80}}>Shop, favourite and review the largest stationery selection right in the palm of your hand.</Text>
+            <Image 
+                source={require("../assets/pi-updates.png")}
+                style={styles.activeScreen}
+            />
+            <View style={styles.btnContainer}>
+                <Button 
+                    title="Let's start"
+                    onPress={() => setShowOnboarding(false)}
+                    titleStyle={{ fontFamily: 'Inter_700Bold', fontSize: 16, textTransform: "uppercase" }}
+                    containerStyle={{
+                        width: "100%",
+                        borderRadius: 6,
+                    }}
+                    buttonStyle={{
+                        backgroundColor: themePalette.primary,
+                        paddingVertical: 15
+                    }}
+                />
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
-    
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        justifyContent: "center",
+        paddingHorizontal: 24,
+        textAlign: "center",
+        alignItems: "center",
+    },
+    onboardImg: {
+        width: 290,
+        height: 238,
+        resizeMode: "contain",
+        marginBottom: 48
+    },
+    activeScreen: {
+        width: 54,
+        height: 8, 
+        marginBottom: 37
+    },
+    btnContainer: {
+        flexDirection: "row",
+        width: "100%",
+        justifyContent: "space-between",
+        gap: 7
+    },
+    btn: {
+        textTransform: "uppercase"
+    }
 });
